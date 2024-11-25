3d803762167a5dd0a1097b30fb955a3e4975a41c
PPKRex <josecarlosppk7@gmail.com>
2024-11-25 16:43:42 +0100

    Created First HTTP Request

--- src/main/java/ppkspringpractices/spring6restmvc/controller/BeerController.java
+++ src/main/java/ppkspringpractices/spring6restmvc/controller/BeerController.java
@@ -3,21 +3,32 @@ package ppkspringpractices.spring6restmvc.controller;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 import ppkspringpractices.spring6restmvc.modal.Beer;
 import ppkspringpractices.spring6restmvc.service.BeerService;
 
+import java.util.List;
 import java.util.UUID;
 
 @Slf4j
 @AllArgsConstructor
-@Controller
+@RestController
+@RequestMapping("/api/v1/beer")
 public class BeerController {
 
     private final BeerService beerService;
 
-    public Beer getBeerById(UUID id){
+    @RequestMapping(method = RequestMethod.GET)
+    public List<Beer> listBeers(){
+        return beerService.listBeers();
+    }
+    @RequestMapping(value = "{beerId}", method = RequestMethod.GET)
+    public Beer getBeerById(@PathVariable("beerId") UUID beerId){
         log.debug("getBeerById - in controller");
 
-        return beerService.getBeerById(id);
+        return beerService.getBeerById(beerId);
     }
 }
--- src/main/java/ppkspringpractices/spring6restmvc/service/BeerService.java
+++ src/main/java/ppkspringpractices/spring6restmvc/service/BeerService.java
@@ -2,8 +2,11 @@ package ppkspringpractices.spring6restmvc.service;
 
 import ppkspringpractices.spring6restmvc.modal.Beer;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface BeerService {
+    List<Beer> listBeers();
+
     Beer getBeerById(UUID id);
 }
